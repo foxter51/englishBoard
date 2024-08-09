f37dc16616278b41df1e573246b44c03e41a0ce1
Ihor Fedorchenko <ihor.fedorchenko.if@gmail.com>
2024-08-09 16:26:46 +0300

    Base structure + auth

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
-class ApplicationController < ActionController::Base
+# frozen_string_literal: true
+
+# Represents the base controller
+class ApplicationController < ActionController::API
+  include JwtToken
+  before_action :authenticate_user
+
+  private
+
+  def authenticate_user
+    header = request.headers['Authorization']
+    if header
+      token = header.split(' ').last
+      begin
+        @decoded = jwt_decode(token)
+        user_id = @decoded[:user_id]
+        if user_id
+          @current_user = User.find(user_id)
+        else
+          render json: { error: 'Invalid token: user_id not found' }, status: :unauthorized
+        end
+      rescue JWT::DecodeError, JWT::ExpiredSignature
+        render json: { error: 'Invalid or expired token' }, status: :unauthorized
+      rescue Mongoid::Errors::InvalidFind, Mongoid::Errors::DocumentNotFound
+        render json: { error: 'User not found' }, status: :unauthorized
+      end
+    else
+      render json: { error: 'Authorization header missing' }, status: :unauthorized
+    end
+  end
 end
---
+++ backend/app/models/user.rb
@@ -0,0 +1,25 @@
+class User
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  
+  field :name, type: String
+  field :string, type: String
+  field :email, type: String
+  field :password_diggest, type: String
+
+  attr_accessor :password
+
+  before_save :encrypt_password
+
+  def authenticate(password)
+    BCrypt::Password.new(password_diggest) == password
+  end
+
+  private
+
+  def encrypt_password
+    if password.present?
+      self.password_diggest = BCrypt::Password.create(password)
+    end
+  end
+end
