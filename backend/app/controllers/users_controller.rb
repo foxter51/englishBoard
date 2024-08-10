9f38fa0badc2e652e650fb321b97809004648d8c
Ihor Fedorchenko <ihor.fedorchenko.if@gmail.com>
2024-08-10 09:27:58 +0300

    Controller logic improvement

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -2,16 +2,15 @@
 
 # Represents the users controller
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show update destroy]
+
   def index
     @users = User.all
     render json: @users
   end
 
   def show
-    @user = User.find(params[:id])
     render json: @user
-  rescue Mongoid::Errors::DocumentNotFound
-    render json: { error: 'User not found' }, status: :not_found
   end
 
   def create
@@ -24,7 +23,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       render json: @user
     else
@@ -33,12 +31,17 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
     head :no_content
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  rescue Mongoid::Errors::DocumentNotFound
+    render json: { error: 'User not found' }, status: :not_found
+  end
 
   def user_params
     params.require(:user).permit(:email, :password, :name)
