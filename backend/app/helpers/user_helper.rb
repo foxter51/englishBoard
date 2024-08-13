b19c07fa8a4f25313ca7e7c82dac0b151daf7656
Ihor Fedorchenko <ihor.fedorchenko.if@gmail.com>
2024-08-13 13:34:36 +0300

    Remove debug message

--- backend/app/helpers/user_helper.rb
+++ backend/app/helpers/user_helper.rb
@@ -5,7 +5,6 @@ module UserHelper
   def fetching_users
     users = $redis.get('all_users') rescue StandardError
     if users.nil?
-      puts('gg')
       users = User.all.to_json
       $redis.set('all_users', users)
       $redis.expire('all_users', 15.seconds.to_i)
